f1e1f879f56850e9c4e8f8abd4bc59ac1f008930
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2024-07-06 15:25:40 -0600

    Cambiar el sistema de navegación para que use componentes (Switch)

--- src/main.jsx
+++ src/main.jsx
@@ -2,54 +2,35 @@ import 'animate.css'
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import { Root, Home, Campaign, Ranked, Profile, Ranking, Registration } from './screens'
 
 const queryClient = new QueryClient()
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/home",
-        element: <Home />,
-        children: [
-          {
-            path: "/home/campaign",
-            element: <Campaign />
-          },
-          {
-            path: "/home/ranked",
-            element: <Ranked />
-          }
-        ]
-      },
-      {
-        path: "/ranking",
-        element: <Ranking />
-      },
-      {
-        path: "/profile",
-        element: <Profile />
-      },
-      {
-        path: "/registration",
-        element: <Registration />
-      }
-    ]
-  }
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route path="home/" element={<Home />}>
+              <Route path="campaign" element={<Campaign />} />
+
+              <Route path="ranked" element={<Ranked />} />
+            </Route>
+
+            <Route path="ranking" element={<Ranking />} />
+
+            <Route path="profile" element={<Profile />} />
+          </Route>
+
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Toaster position="bottom-center" />
     </QueryClientProvider>
   </React.StrictMode>
 )
