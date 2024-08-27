295f9c307ab99ca1361c531cd1fdf42d51b12717
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2024-08-27 01:44:21 -0600

    Añadir funcionalidad de cambiar contraseña

--- src/components/ChangePasswordModal.jsx
+++ src/components/ChangePasswordModal.jsx
@@ -1,10 +1,88 @@
-import { useState } from 'react'
-import { KeyRound } from 'lucide-react'
-import { Modal, PasswordInput, Button } from './'
+import { useMutation } from '@tanstack/react-query'
+import { usePlayer } from '../contexts/player-context'
+import useFormSetters from '../hooks/use-form-setters'
+import toast from '../utils/toast'
+import validator from 'validator'
+import { AuthClient, readClientError } from '../clients'
+import { KeyRound, WifiOff, Ban, Check } from 'lucide-react'
+import {
+  Modal,
+  PasswordInput,
+  Button,
+  Spinner
+} from './'
 
 export default ({ isOpen, onClose }) => {
-  const [oldPassword, setOldPassword] = useState("")
-  const [newPassword, setNewPassword] = useState("")
+  const { player } = usePlayer()
+
+  const formValidator = ({ oldPassword, newPassword }) => {
+    if (validator.isEmpty(oldPassword)) {
+      return "Ingresa tu contraseña actual"
+    }
+
+    if (validator.isEmpty(newPassword)) {
+      return "Ingresa una nueva contraseña"
+    }
+
+    return null
+  }
+
+  const [formState, createFormSetter, formError] = useFormSetters({
+    oldPassword: "",
+    newPassword: ""
+  }, formValidator)
+
+  const handleOnMutate = () => {
+    toast(
+      <Spinner size={40} />,
+      "Actualizando contraseña",
+      "Espera un momento"
+    )
+  }
+
+  const handleOnError = (error) => {
+    const [httpError, serverError] = readClientError(error)
+
+    if (httpError) {
+      toast(
+        <WifiOff size={40} />,
+        "Error de conexión",
+        "Revisa tu conexión a Internet"
+      )
+
+      return
+    }
+
+    if (serverError) {
+      toast(
+        <Ban size={40} />,
+        "Contraseña actual incorrecta",
+        "Revisa que esté bien escrita"
+      )
+
+      return
+    }
+  }
+
+  const handleOnSuccess = () => {
+    toast(
+      <Check size={40} />,
+      "Éxito",
+      "Contraseña actualizada con éxito"
+    )
+    onClose()
+  }
+
+  const changePasswordMutation = useMutation({
+    mutationFn: ({ oldPassword, newPassword }) => AuthClient.changePassword(player?.playerId, oldPassword, newPassword),
+    onMutate: handleOnMutate,
+    onError: handleOnError,
+    onSuccess: handleOnSuccess
+  })
+
+  const handleChange = () => {
+    changePasswordMutation.mutate(formState)
+  }
 
   return (
     <Modal
@@ -15,20 +93,25 @@ export default ({ isOpen, onClose }) => {
     >
       <PasswordInput
         placeholder="Actual contraseña"
-        password={oldPassword}
-        onChange={setOldPassword}
+        password={formState.oldPassword}
+        onChange={createFormSetter("oldPassword")}
       />
 
       <PasswordInput
         placeholder="Nueva contraseña"
-        password={newPassword}
-        onChange={setNewPassword}
+        password={formState.newPassword}
+        onChange={createFormSetter("newPassword")}
       />
 
       <Button
         text="Cambiar"
-        onClick={() => console.log("cambiando")}
+        onClick={handleChange}
+        disabled={formError !== null}
       />
+
+      <span className="font-primary text-lg text-_yellow animate__animated animate__fadeIn">
+        {formError}
+      </span>
     </Modal>
   )
 }
